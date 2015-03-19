56a0f1fecbe4c8d0d0dc30703da4184e82574d83
Hugo González Labrador <hglavra@gmail.com>
2015-03-19 21:53:44 +0100

    init

---
+++ core/boot/factories/auth-interceptor.js
@@ -0,0 +1,33 @@
+angular.module('Instanto')
+    .config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.interceptors.push('AuthInterceptor');
+    }])
+
+    .factory('AuthInterceptor', ['$q', '$location', 'ngToast',
+                        function ($q,   $location,   ngToast) {
+
+            return {
+                'responseError': function(response) {
+                    console.log(response);
+                   if (response.status === 401) {
+                    localStorage.removeItem("instanto_token");
+                        console.error('Trying to access an unauthorized route.');
+                        window.location.href = "/login.html";
+                    } else if (response.status === 400) {
+                        console.error(response.data.reason);
+                        ngToast.create({
+                            content: response.data.field + ': ' + response.data.reason,
+                            class: 'danger'
+                        });
+                    } else  if (response.status === 415) {
+                        console.error('Error sending the request... Check the JSON!');
+                    } else {
+                       ngToast.create({
+                            content: response.data,
+                            class: 'danger'
+                        }); 
+                    }
+                    return $q.reject(response);
+                }
+            };
+    }]);
